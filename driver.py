59a2aeab2c5b64a54499ae770e003e623518b854
Ted Wu <tedwu2016@u.northwestern.edu>
2015-05-24 22:50:12 -0500

    finish driver py

--- driver.py
+++ driver.py
@@ -7,8 +7,15 @@ import re
 words = re.findall(r'\w+', open('ctecs.csv').read().lower());
 
 
-most_common_words = Counter(words).most_common(10)
+most_common_words = Counter(words).most_common(5000)
+stop = stopwords.words('english')
+customized_stop_words = []
 
 print 'Most common:'
 for letter, count in most_common_words:
-    print '%s: %7d' % (letter, count)
+    if letter not in stop and letter.isdigit() == False:
+    	print '%s: %7d' % (letter, count)
+
+
+
+
