ac422651fb163f7a5b4e54f227a924a2c41034eb
Michael Wang <michaelwang0712@gmail.com>
2015-05-29 13:32:38 -0500

    Implemented bag of words (sort of)

--- driver.py
+++ driver.py
@@ -1,21 +1,26 @@
 from collections import Counter
 from nltk.corpus import stopwords
 import re
+import json
 
+# generate a list of words by enumerating over all CTECs
+words = re.findall(r'[a-z]+', open('ctecs.csv').read().lower());
 
-
-words = re.findall(r'\w+', open('ctecs.csv').read().lower());
-
-
-most_common_words = Counter(words).most_common(5000)
+# stop words
 stop = stopwords.words('english')
 customized_stop_words = []
 
-print 'Most common:'
-for letter, count in most_common_words:
-    if letter not in stop and letter.isdigit() == False:
-    	print '%s: %7d' % (letter, count)
+# organize by count
+most_common_words = Counter(words).most_common(5000 + len(stop))
 
-
-
-
+bag = {}
+index = 0
+for word, count in most_common_words:
+    if word not in stop:
+    	bag[word] = index
+        index += 1
+    if index == 5000:
+        break
+# dump a bag of words of the 5000 most common words across CTECs
+with open('bag.json', 'w') as fp:
+    json.dump(bag, fp)
