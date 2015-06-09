7d32c5ee29bfacddb29bc952668106aa263731e9
Michael Wang <michaelwang0712@gmail.com>
2015-06-09 11:28:47 -0500

    generate weka file header

--- driver.py
+++ driver.py
@@ -1,17 +1,9 @@
 from collections import Counter
 from nltk.corpus import stopwords
-from pybrain.tools.shortcuts import buildNetwork
-from pybrain.datasets import SupervisedDataSet
-from pybrain.supervised.trainers import BackpropTrainer
-from collections import defaultdict
 import csv
 import re
 import json
 import os.path
-
-
-
-
 
 def readCTEC(data_file):
     columns = defaultdict(list) # each value in each column is appended to a list
@@ -40,15 +32,14 @@ def preprocess(data_file, bag):
 
     return data, scores
 
-
+# if a bag doesn't exist, create it
 if not os.path.exists("bag.json"):
-    print "not found, building"
+    print "bag not found, building"
     # generate a list of words by enumerating over all CTECs
-    words = re.findall(r'[a-z]+', open('ctecs.csv').read().lower());
-    print words
+    words = re.findall(r'[a-z]+', open('ctecs.csv').read().lower())
+
     # stop words
     stop = stopwords.words('english')
-    customized_stop_words = []
 
     # organize by count
     most_common_words = Counter(words).most_common(5000 + len(stop))
@@ -66,17 +57,35 @@ if not os.path.exists("bag.json"):
         bag = json.dump(bag, fp)
 else:
     print "found, importing"
+    # otherwise, load from file
     bag = {}
     with open('bag.json') as fp:
         bag = json.load(fp)
 
-data, scores = preprocess('ctecs.csv', bag)
-ds = SupervisedDataSet(5000, 1)
+# generate a weka file, starting with the header
+weka = open('ctecs.arff', 'w')
+weka.write('@relation training\n')
+weka.write('\n')
 
-for idx, example in enumerate(data):
-    if scores[idx]:
-        ds.addSample(example, (float(scores[idx]),))
+for item in sorted(bag.items(), key=lambda x: x[1]):
+    weka.write('@attribute \'' + item[0] + '\' {0, 1}\n')
+weka.write('\n')
 
-net = buildNetwork(5000, 3, 1, bias=True)
-trainer = BackpropTrainer(net, ds)
-print trainer.train()
+weka.write('@data\n')
+# with open('ctecs.csv') as f:
+#     for line in f:
+#         words =
+
+
+
+
+# data, scores = preprocess('ctecs.csv', bag)
+# ds = SupervisedDataSet(5000, 1)
+#
+# for idx, example in enumerate(data):
+#     if scores[idx]:
+#         ds.addSample(example, (float(scores[idx]),))
+#
+# net = buildNetwork(5000, 3, 1, bias=True)
+# trainer = BackpropTrainer(net, ds)
+# print trainer.train()
